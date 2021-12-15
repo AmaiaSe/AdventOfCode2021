3e9e47a8bedf78d1fd3109276b19daf3c4530d58
AmaiaSe <amaiaserranop@gmail.com>
2021-12-15 21:12:19 +0100

    .

---
+++ Day12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            List<string[]> connections = new List<string[]>();
+
+            while ((data=Console.ReadLine())!="")
+            {
+                string[] connection = data.Split('-');
+                connections.Add(connection);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day15
+{
+    class Program
+    {
+        public static long bestPath { get; set; }
+        static void Main(string[] args)
+        {
+            string data;
+            List<string> matrix = new List<string>();
+
+            while ((data = Console.ReadLine()) != "")
+            {
+                matrix.Add(data);
+            }
+
+            bestPath = 100;
+            Console.WriteLine(FindBestPath(matrix, bestPath, matrix[0].Length - 1, matrix.Count - 1));
+            Console.WriteLine(bestPath);
+        }
+
+        //--- NOT FINISHED ---
+        public static long FindBestPath(List<string> matrix, long bestPath, int x, int y)
+        {
+            if (x <= 1)
+            {
+                return Convert.ToInt32(Convert.ToString(matrix[x][y]));
+            }
+            else if (y <= 1)
+            {
+                return Convert.ToInt32(Convert.ToString(matrix[x][y]));
+            }
+            else
+            {
+                long pasoDerecha = FindBestPath(matrix, bestPath, x - 1, y);
+                long pasoAbajo = FindBestPath(matrix, bestPath, x, y - 1);
+                if (bestPath > (pasoAbajo + pasoDerecha))
+                {
+                    bestPath = pasoAbajo + pasoDerecha;
+                }
+                return pasoDerecha + pasoAbajo;
+            }
+        }
+    }
+}
